4edfab08f9d025718104d6a91d48c96237cc1cad
Alex Bennett <kestralttr@gmail.com>
2017-02-03 17:43:50 -0800

    removes 'https' altogether from the ajax url in the api call

--- frontend/util/ship_api_util.js
+++ frontend/util/ship_api_util.js
@@ -3,7 +3,7 @@
 export const fetchShip = (id,success) => {
   $.ajax({
     type: "GET",
-    url: `https://swapi.co/api/starships/${id}`,
+    url: `//swapi.co/api/starships/${id}`,
     success: success
   });
 };
